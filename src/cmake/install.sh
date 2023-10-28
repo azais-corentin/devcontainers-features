839af72c0e5f42a5bf0607043048d0ef54a7cc17
AZAIS Corentin <haellsigh@gmail.com>
2023-10-28 20:31:51 +0000

    Go back to symlink

--- src/cmake/install.sh
+++ src/cmake/install.sh
@@ -24,19 +24,6 @@ case "${architecture}" in
         ;;
 esac
 
-# Utilities
-updaterc() {
-    if [ "${UPDATE_RC}" = "true" ]; then
-        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
-        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
-            echo -e "$1" >> /etc/bash.bashrc
-        fi
-        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
-            echo -e "$1" >> /etc/zsh/zshrc
-        fi
-    fi
-}
-
 # Install cmake
 wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-${ARCH}.sh -O /tmp/cmake-install.sh
 chmod u+x /tmp/cmake-install.sh
@@ -44,11 +31,8 @@ mkdir -p /opt/cmake
 /tmp/cmake-install.sh --skip-license --prefix=/opt/cmake
 
 # Add to bashrc/zshrc files
-updaterc "$(cat << EOF
-export CMAKE_HOME="/opt/cmake"
-if [[ "\${PATH}" != *"\${CMAKE_HOME}/bin"* ]]; then export PATH="\${CMAKE_HOME}/bin:\${PATH}"; fi
-EOF
-)"
+ln -s /opt/cmake/bin/cmake /usr/bin/cmake
+ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 # Cleanup
 rm /tmp/cmake-install.sh
