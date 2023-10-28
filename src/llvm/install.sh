91a177193f2c859ecb04ce63530096e2abb66013
AZAIS Corentin <haellsigh@gmail.com>
2023-10-28 18:18:04 +0000

    Fix llvm test

--- src/llvm/install.sh
+++ src/llvm/install.sh
@@ -3,17 +3,13 @@ echo "Activating feature 'llvm'"
 
 LLVM_VERSION=${VERSION:-undefined}
 
-# Install dependencies
-apt-get -qq update
-apt-get -qqy install lsb-release wget software-properties-common gnupg
-
 # Install LLVM
-wget -O /tmp/llvm.sh "https://apt.llvm.org/llvm.sh"
-chmod +x /tmp/llvm.sh
-/tmp/llvm.sh ${LLVM_VERSION}
+echo "deb https://apt.llvm.org/bookworm llvm-toolchain-bookworm-${LLVM_VERSION} main" > /etc/apt/sources.list.d/llvm.list
+wget https://apt.llvm.org/llvm-snapshot.gpg.key -qO /etc/apt/trusted.gpg.d/llvm.asc
+apt-get -qq update
+apt-get -qqy install -t llvm-toolchain-bookworm-${LLVM_VERSION} clang-${LLVM_VERSION} clangd-${LLVM_VERSION} clang-tidy-${LLVM_VERSION} clang-format-${LLVM_VERSION} lld-${LLVM_VERSION}
 
 # Cleanup
 apt-get -qqy autoremove
 apt-get -qqy clean
 rm -rf /var/lib/apt/lists/*
-rm /tmp/llvm.sh
--- test/llvm/test.sh
+++ test/llvm/test.sh
@@ -4,7 +4,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "clang-17 available" bash -c "clang17 --version | grep 'clang version 17'"
+check "clang-17 available" bash -c "clang-17 --version | grep 'clang version 17'"
 
 # Report results
 reportResults
