90aba3943a2a2fa47c7cabd19b5475c1afe1e92f
Azais Corentin <Haellsigh@gmail.com>
2023-12-16 00:16:46 +0100

    Use automatic installation script for llvm by default

--- src/llvm/NOTES.md
+++ src/llvm/NOTES.md
@@ -1,5 +1,3 @@
-
-
 ## Installation prefix
 
 The toolchain is installed with a version suffix.
@@ -8,5 +6,5 @@ For LLVM 17, you can access clang with command `clang-17`.
 If you would like to bypass this installation prefix,
 you can use the following command (adjust `17` to whatever you installed)
 ```
-for f in /usr/lib/llvm-17/bin/*; do ln -sf "$f" /usr/bin; done && \
+for f in /usr/lib/llvm-17/bin/*; do ln -sf "$f" /usr/bin; done
 ```
--- src/llvm/install.sh
+++ src/llvm/install.sh
@@ -6,10 +6,14 @@ echo "Activating feature 'llvm'"
 LLVM_VERSION=${VERSION:-undefined}
 
 # Install LLVM
-echo "deb https://apt.llvm.org/bookworm llvm-toolchain-bookworm-${LLVM_VERSION} main" > /etc/apt/sources.list.d/llvm.list
-wget https://apt.llvm.org/llvm-snapshot.gpg.key -qO /etc/apt/trusted.gpg.d/llvm.asc
-apt-get -qq update
-apt-get -qqy install -t llvm-toolchain-bookworm-${LLVM_VERSION} clang-${LLVM_VERSION} clangd-${LLVM_VERSION} clang-tidy-${LLVM_VERSION} clang-format-${LLVM_VERSION} lld-${LLVM_VERSION}
+if [[ "${LLVM_VERSION}" == "latest" ]]; then
+    bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+else
+    echo "deb https://apt.llvm.org/bookworm llvm-toolchain-bookworm-${LLVM_VERSION} main" > /etc/apt/sources.list.d/llvm.list
+    wget https://apt.llvm.org/llvm-snapshot.gpg.key -qO /etc/apt/trusted.gpg.d/llvm.asc
+    apt-get -qq update
+    apt-get -qqy install -t llvm-toolchain-bookworm-${LLVM_VERSION} clang-${LLVM_VERSION} clangd-${LLVM_VERSION} clang-tidy-${LLVM_VERSION} clang-format-${LLVM_VERSION} lld-${LLVM_VERSION}
+fi
 
 # Cleanup
 apt-get -qqy autoclean
