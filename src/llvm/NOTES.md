e9e99857ad483ab166ee3f3c485ff104a1dc366b
AZAIS Corentin <haellsigh@gmail.com>
2024-02-24 11:30:35 +0100

    Update llvm to install all packages

--- src/llvm/NOTES.md
+++ src/llvm/NOTES.md
@@ -1,10 +1,10 @@
 ## Installation prefix
 
 The toolchain is installed with a version suffix.
-For LLVM 17, you can access clang with command `clang-17`.
+For example for LLVM 17, you can access clang with command `clang-17`.
 
-If you would like to bypass this installation prefix,
-you can use the following command (adjust `17` to whatever you installed)
+If you would like to remove this installation prefix,
+you can use the following command (adjust `17` to the version you installed):
 ```
 for f in /usr/lib/llvm-17/bin/*; do ln -sf "$f" /usr/bin; done
 ```
--- src/llvm/install.sh
+++ src/llvm/install.sh
@@ -6,15 +6,16 @@ echo "Activating feature 'llvm'"
 LLVM_VERSION=${VERSION:-undefined}
 
 # Install LLVM
+apt-get -qq update
+apt-get -qqy install lsb-release wget software-properties-common gnupg
+mkdir -p /tmp
+wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
+chmod +x /tmp/llvm.sh
+
 if [[ "${LLVM_VERSION}" == "latest" ]]; then
-    apt-get -qq update
-    apt-get -qqy install lsb-release wget software-properties-common gnupg
-    bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+    /tmp/llvm.sh all
 else
-    echo "deb https://apt.llvm.org/bookworm llvm-toolchain-bookworm-${LLVM_VERSION} main" > /etc/apt/sources.list.d/llvm.list
-    wget https://apt.llvm.org/llvm-snapshot.gpg.key -qO /etc/apt/trusted.gpg.d/llvm.asc
-    apt-get -qq update
-    apt-get -qqy install -t llvm-toolchain-bookworm-${LLVM_VERSION} clang-${LLVM_VERSION} clangd-${LLVM_VERSION} clang-tidy-${LLVM_VERSION} clang-format-${LLVM_VERSION} lld-${LLVM_VERSION}
+    /tmp/llvm.sh ${LLVM_VERSION} all
 fi
 
 # Cleanup
@@ -22,3 +23,4 @@ apt-get -qqy autoclean
 apt-get -qqy autoremove
 apt-get -qqy clean
 rm -rf /var/lib/apt/lists/*
+rm /tmp/llvm.sh
