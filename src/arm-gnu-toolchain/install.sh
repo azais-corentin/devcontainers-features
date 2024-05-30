fd52c95de6bbf6cf7912964645964ef74541bf90
AZAIS Corentin <haellsigh@gmail.com>
2024-05-30 18:34:11 +0000

    Temporarily disable checksum verification for arm gnu toolchain

--- src/arm-gnu-toolchain/install.sh
+++ src/arm-gnu-toolchain/install.sh
@@ -28,8 +28,9 @@ cd /tmp/arm-gnu-toolchain
 apt-get -qq update
 apt -qqy --no-install-recommends install python3 python3-pip libncursesw5
 wget -qP /tmp/arm-gnu-toolchain --show-progress "https://developer.arm.com/-/media/Files/downloads/gnu/${ARM_GNU_TOOLCHAIN_VERSION}/binrel/arm-gnu-toolchain-${ARM_GNU_TOOLCHAIN_VERSION}-x86_64-arm-none-eabi.tar.xz"
-wget -qO /tmp/arm-gnu-toolchain/archive.md5 --show-progress "https://developer.arm.com/-/media/Files/downloads/gnu/${ARM_GNU_TOOLCHAIN_VERSION}/binrel/arm-gnu-toolchain-${ARM_GNU_TOOLCHAIN_VERSION}-x86_64-arm-none-eabi.tar.xz.asc"
-md5sum -c /tmp/arm-gnu-toolchain/archive.md5
+# Temporarily disable checksum verification since the official links do not work anymore
+# wget -qO /tmp/arm-gnu-toolchain/archive.md5 --show-progress "https://developer.arm.com/-/media/Files/downloads/gnu/${ARM_GNU_TOOLCHAIN_VERSION}/binrel/arm-gnu-toolchain-${ARM_GNU_TOOLCHAIN_VERSION}-x86_64-arm-none-eabi.tar.xz.asc"
+# md5sum -c /tmp/arm-gnu-toolchain/archive.md5
 tar xf /tmp/arm-gnu-toolchain/*.xz -C /opt
 
 # Cleanup
